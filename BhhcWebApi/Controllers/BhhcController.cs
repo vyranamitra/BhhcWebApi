8795397337f937d3305da2c99feaf1eb5656ffc5
vrana-mitra <vrana-mitra@varis1.com>
2020-02-16 12:33:20 -0800

    a

--- BhhcWebApi/Controllers/BhhcController.cs
+++ BhhcWebApi/Controllers/BhhcController.cs
@@ -1,9 +1,5 @@
 ﻿using BhhcWebApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
---
+++ BhhcWebApi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BhhcWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        // GET: Home
+        public IHttpActionResult Index()
+        {
+            HttpContext httpContext = HttpContext.Current;
+            return Ok("Hello World!!");
+        }
+    }
+}
